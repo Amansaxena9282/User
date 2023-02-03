3bbb9b8c49b84941dc59d0f26dbe7814b471548b
Aman <Aman123@gmail.com>
2023-02-03 22:27:29 +0530

    first commit

--- src/main/java/com/training/Server/ServerApplication.java
+++ src/main/java/com/training/Server/ServerApplication.java
@@ -3,8 +3,9 @@ package com.training.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplicationgi
 public class ServerApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
